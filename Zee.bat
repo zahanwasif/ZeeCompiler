9cb7ee63c3860b0e7d73c7f6b399ec7fafd8e7d3
zahanwasif <21100213@lums.edu.pk>
2020-05-15 18:31:12 +0500

    Adding bin folder

--- Zee.bat
+++
@@ -1,6 +0,0 @@
-@ECHO off
-SETLOCAL ENABLEDELAYEDEXPANSION
-set myvar=PATH
-FOR %%i IN ("%~dp0..") DO SET ZEE_ROOT=%%~fi
-SET "FILE_PATH = "%ZEE_ROOT%\util""
-"C:\Users\zahan\AppData\Local\Microsoft\WindowsApps\python3.exe" ""%CD%"\parser.py"
---
+++ bin/Zee.bat
@@ -0,0 +1,8 @@
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR %%i IN ("%~dp0..") DO SET ZEE_ROOT=%%~fi
+echo %ZEE_ROOT%
+echo "Zee language Compiler made by Zahan Wasif"
+more %ZEE_ROOT%\LICENSE
+python3 %ZEE_ROOT%\util\parser.py
+pause
